cc14e328a0d30357b089e3cdb7aa6df0551da82c
katiaperchet <katiamperchet@gmail.com>
2020-01-22 21:48:02 -0300

    Added class and corrected the name of the page

---
+++ WebActividades/WebActividades/Models/Actividad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebActividades.Models
+{
+	public class Actividad
+	{
+		public string Area { get; set; }
+		public string Institucion { get; set; }
+		public string Titulo { get; set; }
+		public string Creador { get; set; }
+		public decimal Duracion { get; set; }
+		public string Materiales { get; set; }
+		public string Resumen { get; set; }
+		public string Descripcion { get; set; }
+		public string Pautas { get; set; }
+		public string Naps { get; set; }
+	}
+}
